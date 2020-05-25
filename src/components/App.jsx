232c529789c205e38052989b189531b6b0ff60c9
vimal patel <vimal.patel.nvs@gmail.com>
2020-05-25 09:29:16 +0530

    Routing added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import UserList from './UserList';
 import Login  from './Login';
 import  Navbar  from './Navbar';
+import { Link, BrowserRouter, Route } from 'react-router-dom';
 
 const App = () => {
 	return (
 		<div className="App">
-			<Navbar/>
-			<Login/>
+			<BrowserRouter>
+				<Navbar/>
+                    <Route path='/' render={()=> (<h2>hello</h2>)}/>
+					<Route exact path='/login' component={Login}/>
+			</BrowserRouter>
 		</div>
 	);
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import React,{Component  } from 'react'
 import { connect } from 'react-redux'
+import { BrowserRouter, Link } from 'react-router-dom'
 
 export class Navbar extends Component {
     render() {
         return (
             <div>
                 <h2>React-Redux-App</h2>
+                <div>
+                    <Link to="/">Home</Link>
+                    <Link to="/login">Login</Link>
+                </div>
             </div>
         )
     }
