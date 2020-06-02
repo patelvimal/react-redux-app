93e552c81d8e44368a511fb1c10006766ae2f780
vimal patel <vimal.patel.nvs@gmail.com>
2020-06-02 12:07:54 +0530

    ENvironement wise configuration added

--- src/shared/apis/index.js
+++ src/shared/apis/index.js
@@ -1,7 +1,9 @@
 import Axios from "axios";
 
 //const baseUrl= "https://jsonplaceholder.typicode.com/";
-const baseUrl= "http://localhost:3001/";
+//const baseUrl= "http://localhost:3001/";
+
+const baseUrl  = process.env.REACT_APP_API_ENDPOINT;
 
 export default Axios.create({
     baseURL: baseUrl
